c7f2d9333fa115d228065ab9b31132fda1b844e8
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-12-25 11:46:43 +0500

    a sum of the odd elements

--- task22.py
+++ task22.py
@@ -1,14 +1,20 @@
 import random
 
-num = int(input('Введите количество элементов списка: '))
-list1 = []
-for _ in range(num):
-    list1.append((random.randint(-num, num)))
-print(list1)
+def new_list(n):
+    list1 = []
+    for _ in range(n):
+        list1.append((random.randint(-n, n)))
+    return list1
 
-index = 1
-summa = 0
-while index <= num:
-    summa += list1[index]
-    index += 2
-print('Сумма нечетных элементов = ', summa)
+def sum_odd_elem(list1):
+    index = 1
+    summa = 0
+    while index < len(list1):
+        summa += list1[index]
+        index += 2
+    return summa
+
+num = int(input('Введите количество элементов списка: '))
+my_list = new_list(num)
+print(my_list)
+print('Сумма нечетных элементов = ', sum_odd_elem(my_list))
